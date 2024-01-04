511c54346f93b5ba9ca2d69680716afd444fef57
sambajr <metasamba.ng007@gmail.com>
2024-01-04 20:43:14 +0100

    0. Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,20 @@
 #!/usr/bin/python3
+# Import the required function
+# Import the required function
 from add_0 import add
-a = 1
-b = 2
-from add_0 import add
-print("{} + {} = {}".format(a, b, add(a, b)))
+
+
+def main():
+    # Define the variables
+    a, b = 1, 2
+
+    # Compute the sum
+    sum_ab = add(a, b)
+
+    # Print the result in the required format
+    print(f"{a} + {b} = {sum_ab}")
+
+
+# Ensure the main function is only executed when this script is run directly
+if __name__ == "__main__":
+    main()
