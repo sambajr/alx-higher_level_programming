252d587016310d8eb846130f5be8d3e89e0d8dd3
sambajr <metasamba.ng007@gmail.com>
2024-01-04 23:27:43 +0100

    Import a simple function from a simple file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-# Import the required function
-# Import the required function
 from add_0 import add
 
-
 if __name__ == "__main__":
-    # Define the variables
+    # Assign values to variables a and b
     a = 1
     b = 2
 
-    # Compute the sum
-    sum_ab = add(a, b)
-
-    # Print the result in the required format
-    print(f"{a} + {b} = {sum_ab}")
+    # Print the result of the addition
+    print(f"{a} + {b} = {add(a, b)}")
