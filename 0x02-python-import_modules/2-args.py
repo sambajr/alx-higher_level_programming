573777967b432dece6af27c28aa1a5cfcec31d25
sambajr <metasamba.ng007@gmail.com>
2024-01-04 21:52:26 +0100

    2. How to make a script dynamic!

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    # Get the number of arguments
+    num_args = len(sys.argv) - 1  # Subtract 1 to exclude the script name
+
+    # Print the number of arguments
+    print(f"{num_args} arguments", end="")
+
+    if num_args == 0:
+        print(".")  # If no arguments, print a dot and a new line
+    else:
+        print(":")  # If at least one argument, print a colon and a new line
+
+        # Print one line per argument
+        for i in range(1, num_args + 1):
+            print(f"{i}: {sys.argv[i]}")
