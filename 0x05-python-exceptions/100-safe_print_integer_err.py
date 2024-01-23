9b15657c4d24c81ee5315f4a81e02c1dec67c38e
sambajr <metasamba.ng007@gmail.com>
2024-01-23 23:03:16 +0100

    8. Safe function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+import sys
+
+
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
+        integer_value = int(value)
+        print("{:d}".format(integer_value))
         return True
-    except Exception as e:
-        print("Exception: {}".format(e))
+    except ValueError as e:
+        print("Exception:", e, file=sys.stderr)
         return False
