a13b5e4061aaede6b9b43c2ba2dcf98f4ded9107
sambajr <metasamba.ng007@gmail.com>
2024-01-23 22:19:23 +0100

    3. Integers division with debug

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+        print("Inside result: {}".format(result))
+        return result
+    except ZeroDivisionError:
+        return 1
+    finally:
+        if 'result' not in locals():
+            print("Inside result: None")
