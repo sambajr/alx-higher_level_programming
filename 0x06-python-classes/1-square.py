6dac3acd9c3b57c2a2138e50a4bf2f01f9a7b464
sambajr <metasamba.ng007@gmail.com>
2024-02-04 20:58:45 +0100

    a class Square that defines a square by: (based on 0-square.py)

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Square Module
+"""
+
+
+class Square:
+    """
+    Square Module
+    """
+    def __init__(self, size):
+        """
+        Initializes the Square instance with a size.
+        """
+        self.__size = size
